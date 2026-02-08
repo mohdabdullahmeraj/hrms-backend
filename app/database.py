62cbf8d12914a7803cac30ad959395c3bf71ec4d
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2026-02-08 17:08:23 +0530

    deploy db link

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Abdullah_170505@host.docker.internal/hrms_db"
+# DATABASE_URL = "mysql+pymysql://root:Abdullah_170505@host.docker.internal/hrms_db"
+DATABASE_URL = "mysql+pymysql://4YqJDtkonYkehfW.root:GsNlcBBSC4RVDLTG@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=<CA_PATH>&ssl_verify_cert=true&ssl_verify_identity=true"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
